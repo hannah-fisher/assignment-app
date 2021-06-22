ddbc519cf82585e6052ee924b430137001d6f11d
Hannah <hannahmaryfisher@gmail.com>
2021-06-21 21:22:25 -0600

    Improve assignment display.

--- AssignmentWidget.py
+++ AssignmentWidget.py
@@ -6,6 +6,26 @@ class AssignmentWidget(QtWidgets.QWidget):
     def __init__(self, assignment):
         super().__init__()
         self.assignment = assignment
-        label = QtWidgets.QLabel(self)
-        label.setText(assignment.title + ", " + str(assignment.due_date))
-        # TODO a much better display
+        self.label = QtWidgets.QLabel(self)
+        self.set_label_text()
+        self.setStyleSheet("""
+            QLabel::hover {
+                background-color: red;
+            }
+        """)
+        self.setFixedHeight(40) # should do this as fraction of window height
+
+    def set_label_text(self):
+        assignment = self.assignment
+        text = ""
+        text += "!" * assignment.priority
+        if text != "":
+            text += " "
+        text += assignment.title
+        text += " due "
+        text += str(assignment.due_date.month) + "/" + str(assignment.due_date.day)
+        text += " at " + str(assignment.due_date.hour % 12)
+        text += " am" if assignment.due_date.hour < 12 else " pm"
+        if assignment.notes != "":
+            text += " ..."
+        self.label.setText(text)
--- CategoryWidget.py
+++ CategoryWidget.py
@@ -1,4 +1,5 @@
 from PySide6 import QtWidgets
+from PySide6.QtCore import Qt
 
 from Style import Style
 
@@ -11,9 +12,22 @@ class CategoryWidget(QtWidgets.QWidget):
         label.setText(name)
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(label)
+
+        self.scroll = QtWidgets.QScrollArea()
+        self.widget = QtWidgets.QWidget()
+        self.vbox = QtWidgets.QVBoxLayout()
+        self.widget.setLayout(self.vbox)
+
+        self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scroll.setWidgetResizable(True)
+        self.scroll.setWidget(self.widget)
+
+        self.layout.addWidget(self.scroll)
         self.setLayout(self.layout)
+
         Style.backgroundColorWidget(self, color)
 
     def add_assignment(self, assignment):
         assignmentWidget = AssignmentWidget(assignment)
-        self.layout.addWidget(assignmentWidget)
+        self.vbox.addWidget(assignmentWidget)
